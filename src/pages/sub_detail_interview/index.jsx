a6133534f5411579e19b0ee26c58996b4f840ea8
huiergo <928187057@qq.com>
2022-03-03 11:19:40 +0800

    feat: sub interview detail

--- src/pages/sub_detail_interview/index.jsx
+++ src/pages/sub_detail_interview/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Taro from '@tarojs/taro';
 import { View, Image, Button } from '@tarojs/components';
+import apis from '../../services/apis';
+import { getJSON } from "../../services/method";
+
 import './index.scss'
 
 const Split = () => {
@@ -8,74 +11,117 @@ const Split = () => {
   </View>
 }
 
-const html = `<p><b>面试公司：</b>广州欣芝妍化妆品有限公司<br></p><p><b>面试岗位：</b>前端开发</p><p><br></p><p>
-</p><p><b>面试问题：
-</b></p><p>1.<a href="https://www.itcast.cn/news/20211007/11050312845.shtml" target="_blank"> let、const、var区别</a>
-</p><p>2. 是否使用过vuex，vuex里面的几个核心模块有哪些
-</p><p>3. <a href="https://www.itcast.cn/news/20180105/13395719059.shtml" target="_blank">闭包</a>
-</p><p>4. <a href="https://www.itheima.com/news/20211019/110800.html" target="_blank">原型链</a>
-</p><p>5. <a href="https://www.itcast.cn/news/20201016/11023220300.shtml" target="_blank">深拷贝的方</a>法（json方法的缺点：不能拷贝函数和 undefined）
-</p><p>6. <a href="https://www.itcast.cn/news/20210929/17580567129.shtml" target="_blank">vue生命周期</a>（mounted和created的区别，mounted是否任何情况下都能操作到dom）
-</p><p>7. 深拷贝如何实现
-</p><p>8. <a href="https://www.itcast.cn/news/20210506/15001052902.shtml" target="_blank">基本数据类型</a>
-</p><p>9. 基本数据类型和引用数据类型你是怎么理解的
-</p><p>10. 简述项目中遇到的问题，如何解决
-</p><p>11. 如何判断数据类型（typeof和instanceof、对象的constructor）
-</p><p>12. 职业发展规划</p><p>
-</p><p>`
+// const html = `<p><b>面试公司：</b>广州欣芝妍化妆品有限公司<br></p><p><b>面试岗位：</b>前端开发</p><p><br></p><p>
+// </p><p><b>面试问题：
+// </b></p><p>1.<a href="https://www.itcast.cn/news/20211007/11050312845.shtml" target="_blank"> let、const、var区别</a>
+// </p><p>2. 是否使用过vuex，vuex里面的几个核心模块有哪些
+// </p><p>3. <a href="https://www.itcast.cn/news/20180105/13395719059.shtml" target="_blank">闭包</a>
+// </p><p>4. <a href="https://www.itheima.com/news/20211019/110800.html" target="_blank">原型链</a>
+// </p><p>5. <a href="https://www.itcast.cn/news/20201016/11023220300.shtml" target="_blank">深拷贝的方</a>法（json方法的缺点：不能拷贝函数和 undefined）
+// </p><p>6. <a href="https://www.itcast.cn/news/20210929/17580567129.shtml" target="_blank">vue生命周期</a>（mounted和created的区别，mounted是否任何情况下都能操作到dom）
+// </p><p>7. 深拷贝如何实现
+// </p><p>8. <a href="https://www.itcast.cn/news/20210506/15001052902.shtml" target="_blank">基本数据类型</a>
+// </p><p>9. 基本数据类型和引用数据类型你是怎么理解的
+// </p><p>10. 简述项目中遇到的问题，如何解决
+// </p><p>11. 如何判断数据类型（typeof和instanceof、对象的constructor）
+// </p><p>12. 职业发展规划</p><p>
+// </p><p>`
 
-let item = {
-  views: 367,
-  zanNum: 10,
-  avatar: 'https://img2.baidu.com/it/u=1028277752,678118340&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
-  title: 'interview组件的优势是什么？',
-  date: '2020-02-02',
-  content: '巴拉了解到付款冷冻机房加大加肥禄口街道复健科多了几分两道算法离开家巴拉了解到付款冷冻机房加大加肥禄口街道复健科多了几分两道算法离开家巴拉了解到付款冷冻机房加大加肥禄口街道复健科多了几分两道算法离开家',
-  author: '油炸小饭团'
-}
+// let item = {
+//   views: 367,
+//   zanNum: 10,
+//   avatar: 'https://img2.baidu.com/it/u=1028277752,678118340&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
+//   title: 'interview组件的优势是什么？',
+//   date: '2020-02-02',
+//   content: '巴拉了解到付款冷冻机房加大加肥禄口街道复健科多了几分两道算法离开家巴拉了解到付款冷冻机房加大加肥禄口街道复健科多了几分两道算法离开家巴拉了解到付款冷冻机房加大加肥禄口街道复健科多了几分两道算法离开家',
+//   author: '油炸小饭团'
+// }
 class SubDetail extends Component {
   constructor() {
     super(...arguments)
+    this.state = {
+      item: {}
+    }
   }
   // onLoad
-  onLoad(options) {
-    console.log("[SubDetail interview------]", options)
-    const { } = options
+  // onLoad
+  async onLoad(options) {
+    const { id } = options
+    console.log("[SubDetail questionn------]", id)
+    await this.initSubInterviewDetail(id)
   }
 
+  async initSubInterviewDetail(id) {
+    let result = await getJSON(apis.getQuestionDetail, { id });
+    if (result && result.data && result.data.data) {
+      this.setState({
+        item: result.data.data
+      })
+    }
+  }
+
+
   // 赞 事件
-  handleZan() {
-    console.log("点赞id----", this.props.currentId)
-    // this.props.currentId
+  async handleZan(flag) {
+    console.log("点赞id----", this.state.item.id)
+    // todo: 这里type先写死
+    let result = this.unitOptRequest({ action: flag ? 'unOpt' : 'opt', id: this.state.item.id, type: 1, optType: 1 })
+    result && this.setState({
+      item: {
+        ...this.state.item,
+        likeFlag: !flag
+      }
+    })
   }
   // 收藏 事件
-  handleFavorite() {
-    console.log("收藏 id----", this.props.currentId)
-    // this.props.currentId
+  handleFavorite(flag) {
+    console.log("收藏 id----", this.state.item.id)
+    // todo: 这里type先写死
+    let result = this.unitOptRequest({ action: flag ? 'unOpt' : 'opt', id: this.state.item.id, type: 1, optType: 2 })
+    result && this.setState({
+      item: {
+        ...this.state.item,
+        collectFlag: !flag
+      }
+    })
+  }
+  /**
+   * type:     0面试题1面经
+   * optType:  1点赞2收藏3浏览
+   */
+  async unitOptRequest({ action, id, type = 1, optType }) {
+    let api = action == 'opt' ? apis.opt : apis.unOpt
+    let result = await getJSON(api, { id, type, optType });
+    if (result && result.data && result.data.data) {
+      return true
+    }
   }
 
   render() {
-    const { date, views, zanNum, author, avatar } = item
+    let { item } = this.state
+    const { createdAt, views, likeCount, creatorName, creatorAvatar, collectFlag, likeFlag } = item
+
     return (
       <View className='detail-interview'>
         <View className='detail-interview__title'>腾讯前端三面挂</View>
         <View className='detail-interview__des'>
-          {date}
+          {createdAt}
           <Split />
           {views} 浏览量
           <Split />
-          {zanNum} 赞
+          {likeCount} 赞
         </View>
         <View className='detail-interview__user'>
-          <Image className='detail-interview__user-avatar' src={avatar} />
-          {author}
+          <Image className='detail-interview__user-avatar' src={creatorAvatar} />
+          {creatorName}
         </View>
-        <View className='detail-content' dangerouslySetInnerHTML={{ __html: html }}></View>
+        {/* <View className='detail-content' dangerouslySetInnerHTML={{ __html: html }}></View> */}
+        <View className='detail-content'>{item.content}</View>
 
         {/* 点赞和收藏按钮 */}
         <View className='zan-favorite-btns'>
-          <Image className='zan-btn' src={require('../../assets/zan.png')} onClick={this.handleZan} />
-          <Image className='favorite-btn' src={require('../../assets/favorite-icon.png')} onClick={this.handleFavorite} />
+          <Image className='zan-btn' src={likeFlag ? require('../../assets/zan.png') : require('../../assets/delete.png')} onClick={() => this.handleZan(likeFlag)} />
+          <Image className='favorite-btn' src={collectFlag ? require('../../assets/favorite-icon.png') : require('../../assets/delete.png')} onClick={() => this.handleFavorite(collectFlag)} />
         </View>
 
       </View>
--- src/pages/sub_detail_question/index.jsx
+++ src/pages/sub_detail_question/index.jsx
@@ -34,34 +34,6 @@ const handleTag = (tag) => {
   }
 }
 
-// let item = {
-//   difficulty: 1,
-//   title: '收藏 题目1-Question的优势是什么？',
-//   likeCount: 666,
-//   views: 99,
-//   likeFlag: true,
-//   stage: 'vue',
-//   currentId: 5,
-//   nextId: 6,
-//   lastId: 4
-// }
-
-// let item1 = {
-//   content: "周华元会原需书县便山式声。济开记们在应林北取太果广身。术毛民少使铁水即增料业位办细。色红这员方市何关万北根过史维难北几。有再土备建值半月可发型子元。"
-// createdAt: "2007-12-08"
-// difficulty: 3
-// id: 310000197711147400
-// likeCount: 605
-// likeFlag: 0
-// planSceneName: "核心提炼"
-// questionNo: "41"
-// questionType: 4
-// stem: "条格议大水米联头因技但严行史务圆"
-// stemAttachmentId: "cid://nmy.hn/guhb"
-// subjectName: "关保件到王"
-// views: 597
-// }
-
 const IconText = ({ title, des }) => {
   return (<View className='icon-text'>
     <View className='vertical-line'></View>
@@ -73,21 +45,7 @@ const IconText = ({ title, des }) => {
 const HorizonLine = () => {
   return (<View className='horizon-line'></View>)
 }
-const html = `<p><b>面试公司：</b>广州欣芝妍化妆品有限公司<br></p><p><b>面试岗位：</b>前端开发</p><p><br></p><p>
-</p><p><b>面试问题：
-</b></p><p>1.<a href="https://www.itcast.cn/news/20211007/11050312845.shtml" target="_blank"> let、const、var区别</a>
-</p><p>2. 是否使用过vuex，vuex里面的几个核心模块有哪些
-</p><p>3. <a href="https://www.itcast.cn/news/20180105/13395719059.shtml" target="_blank">闭包</a>
-</p><p>4. <a href="https://www.itheima.com/news/20211019/110800.html" target="_blank">原型链</a>
-</p><p>5. <a href="https://www.itcast.cn/news/20201016/11023220300.shtml" target="_blank">深拷贝的方</a>法（json方法的缺点：不能拷贝函数和 undefined）
-</p><p>6. <a href="https://www.itcast.cn/news/20210929/17580567129.shtml" target="_blank">vue生命周期</a>（mounted和created的区别，mounted是否任何情况下都能操作到dom）
-</p><p>7. 深拷贝如何实现
-</p><p>8. <a href="https://www.itcast.cn/news/20210506/15001052902.shtml" target="_blank">基本数据类型</a>
-</p><p>9. 基本数据类型和引用数据类型你是怎么理解的
-</p><p>10. 简述项目中遇到的问题，如何解决
-</p><p>11. 如何判断数据类型（typeof和instanceof、对象的constructor）
-</p><p>12. 职业发展规划</p><p>
-</p><p>`
+
 class SubDetail extends Component {
   constructor() {
     super(...arguments)
@@ -99,10 +57,10 @@ class SubDetail extends Component {
   async onLoad(options) {
     const { id } = options
     console.log("[SubDetail questionn------]", id)
-    await this.initSubDetail(id)
+    await this.initSubQuestionDetail(id)
   }
 
-  async initSubDetail(id) {
+  async initSubQuestionDetail(id) {
     let result = await getJSON(apis.getQuestionDetail, { id });
     if (result && result.data && result.data.data) {
       this.setState({
@@ -110,12 +68,6 @@ class SubDetail extends Component {
       })
     }
   }
-
-  // // 上一页下一页点击事件
-  // pageClick(id) {
-  //   console.log("上一页下一页id", id)
-  //   // redirectToPage({ url: `./index?id=${id}` })
-  // }
 
   // 赞 事件
   async handleZan(flag) {
