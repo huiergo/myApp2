6b9cfafbe67a3633e4e930a9dd2a4fee27ccdfe0
huiergo <928187057@qq.com>
2022-03-15 21:28:47 +0800

    update

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   pages: [
-    'pages/first/index',
+    'pages/first-past/index',
     'pages/experience/index',
     'pages/favorite/index',
     'pages/mine/index',
@@ -23,7 +23,7 @@ export default defineAppConfig({
       {
         iconPath: 'assets/bottom_tabs_icons/first.png',
         selectedIconPath: 'assets/bottom_tabs_icons/first_select.png',
-        pagePath: 'pages/first/index',
+        pagePath: 'pages/first-past/index',
         text: '首页',
         selectedColor: '#3C3E42',
       },
