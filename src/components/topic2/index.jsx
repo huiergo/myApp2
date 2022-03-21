1cdace7139d8b45556780b5bdbb241c90bec80e9
huiergo <928187057@qq.com>
2022-03-21 16:36:03 +0800

    feat: update 面经详情 ui

--- src/components/topic2/index.jsx
+++ src/components/topic2/index.jsx
@@ -71,7 +71,7 @@ class Topic extends Component {
     const { type, list, page, pageTotal, initData, loadMore, current, index, optType } = this.props
 
     const dataLen = list.length
-    const itemSize = 140
+    const itemSize = 160
 
     return (
       <View>
