7d900f63a33844d6753b5cf002f16b59e2f88ba1
huiergo <928187057@qq.com>
2022-03-25 14:28:16 +0800

    feat: update remove

--- src/utils/index.js
+++ src/utils/index.js
@@ -64,35 +64,6 @@ export const throttle = (fn, delay, mustRunDelay) => {
   };
 };
 
-// // 防抖
-// function debounce(fn, delay = 300) {
-//   //默认300毫秒
-//   let timer;
-//   return function () {
-//     const args = arguments;
-//     if (timer) {
-//       clearTimeout(timer);
-//     }
-//     timer = setTimeout(() => {
-//       fn.apply(this, args); // 改变this指向为调用debounce所指的对象
-//     }, delay);
-//   };
-// }
-
-// // 节流
-// // 设置一个标志
-// function throttle(fn, delay) {
-//   let flag = true;
-//   return () => {
-//     if (!flag) return;
-//     flag = false;
-//     timer = setTimeout(() => {
-//       fn();
-//       flag = true;
-//     }, delay);
-//   };
-// }
-
 /*获取当前页带参数的url*/
 export const getCurrentPageUrlWithArgs = () => {
   var pages = Taro.getCurrentPages(); //获取加载的页面
